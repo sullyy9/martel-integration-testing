9f5e91a7ae20d7755386f9b760f8f6bb02323618
ryansullivan <ryansullivan@martelinstruments.com>
2022-10-01 11:57:12 +0100

    Implement some library unit testing

---
+++ martel_printer_test_lib/test_printer_mech.py
@@ -0,0 +1,19 @@
+import unittest
+import itertools
+
+from printer_mech import PaperBuffer
+
+
+class TestPaperBuffer(unittest.TestCase):
+    def setUp(self):
+        self.paper_buffer = PaperBuffer()
+
+    def test_new_line(self):
+        self.paper_buffer.new_line()
+        self.assertEqual(len(self.paper_buffer.buffer), 3,
+                         'Incorrect paper buffer length')
+
+        for _ in itertools.repeat(None, 5):
+            self.paper_buffer.new_line()
+        self.assertEqual(len(self.paper_buffer.buffer), 8,
+                         'Incorrect paper buffer length')
