bc28dcee37754de53a0e50bf00640cc23ff26fa2
Ryan Sullivan <ryansully96@googlemail.com>
2022-11-10 16:32:52 +0000

    Increase usage of ndarray's for better performance

--- martel_printer_test_lib/mech/printout_generation.py
+++ martel_printer_test_lib/mech/printout_generation.py
@@ -1,14 +1,13 @@
 import csv
-import math
 from dataclasses import dataclass
-from operator import add
-from collections import deque
 from pathlib import Path
 from typing import Self
 
 import cv2
-import numpy
-from numpy import ndarray, dtype
+
+import numpy as np
+from numpy import uint8, float64
+from numpy.typing import NDArray
 
 from printout import Printout
 
@@ -50,18 +49,18 @@ class MechInputRecords:
 class PrintMechState:
     """
     Simulation of the current state of the printer's print mechanism.
+
     """
+    def __init__(self, initial_input: MechInput) -> None:
+        self.last_input: MechInput = initial_input
+        self.shift_register: NDArray[uint8] = np.zeros(DOTS_PER_LINE, uint8)
+        self.latch_register: NDArray[uint8] = np.zeros(DOTS_PER_LINE, uint8)
+        self.paper: PaperBuffer = PaperBuffer()
 
-    def __init__(self, initial_input: MechInput):
-        self.last_input = initial_input
-        self.shift_register = deque([0] * DOTS_PER_LINE, maxlen=DOTS_PER_LINE)
-        self.latch_register = [0] * DOTS_PER_LINE
-        self.paper = PaperBuffer()
+        self.burn_time: float = 0.0
+        self.motor_steps: int = 0
 
-        self.burn_time = 0.0
-        self.motor_steps = 0
-
-    def update(self, mech_input: MechInput):
+    def update(self, mech_input: MechInput) -> None:
         # DST controls the activation of the thermal head
         # along with the data in the latch register
         if self.last_input.dst == 1:
@@ -70,102 +69,113 @@ class PrintMechState:
         # Data in the shift register is transfered to the latch
         # register when the latch is pulled low.
         if self.last_input.latch == 1 and mech_input.latch == 0:
-            self.burn_shift_register()
-            self.latch_register = list(self.shift_register)
+            self.burn_latch_register()
+            self.latch_register = self.shift_register.copy()
 
         # Data bits are valid on the clock's rising edge.
         # The bits get shifted in to the shift register.
         if mech_input.spi_clock == 1 and self.last_input.spi_clock == 0:
-            self.shift_register.append(mech_input.spi_data)
+            self.shift_register[0:-1] = self.shift_register[1:]
+            self.shift_register[-1] = mech_input.spi_data
 
         # One dot line is 4 steps.
         if mech_input.motor_state != self.last_input.motor_state:
             self.motor_steps += 2  # 2 steps every state change
             if self.motor_steps == 2:
-                self.burn_shift_register(between_lines=True)
+                self.burn_latch_register(between_lines=True)
             elif self.motor_steps >= 4:
-                self.burn_shift_register()
+                self.burn_latch_register()
                 self.advance_line()
                 self.motor_steps = 0
 
         self.last_input = mech_input
 
-    def burn_shift_register(self, between_lines: bool = False):
+    def burn_latch_register(self, between_lines: bool = False) -> None:
         """
-        Burn a dot line into the paper, simulating activation
-        of the thermal head.
+        Burn a dot line into the paper, simulating activation of the thermal
+        head.
+
         """
-        burn_buffer = [
-            float(bit) * self.burn_time for bit in self.latch_register]
+        burn_buffer = self.latch_register * self.burn_time
+
         self.paper.burn_line(burn_buffer, between_lines=between_lines)
         self.burn_time = 0.0
 
-    def advance_line(self):
+    def advance_line(self) -> None:
         """
         Advance the paper by 1 dot line.
+
         """
         self.paper.new_line()
 
     def get_printout(self) -> Printout:
         """
         Return the image that has been burned into the paper.
+
         """
-        self.burn_shift_register()
+        self.burn_latch_register()
         return self.paper.as_printout()
 
 
 class PaperBuffer:
     """
-    TODO Use numpy arrays
-
     2D buffer representing the thermal paper.
 
-    The paper is divided into a grid where each cell is 1 thermal head 'dot' in size.
+    The paper is divided into a grid where each cell is 1 thermal head 'dot' in
+    size. Each element contains the ammount of time a thermal head 'dot' has
+    burned over a cell.
 
-    Each element contains the ammount of time a thermal head 'dot' has burned over a cell.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Initialise the paper buffer with 2 lines.
-        """
-        self.buffer = [[0.0] * DOTS_PER_LINE, [0.0] * DOTS_PER_LINE]
 
-    def new_line(self):
+        """
+        self.buffer: NDArray[float64] = np.zeros([2, DOTS_PER_LINE])
+
+    def new_line(self) -> None:
         """
         Add a new line to the end of the buffer.
 
         Visualy, this line is located at the bottom of the paper.
-        """
-        self.buffer.append([0.0] * DOTS_PER_LINE)
 
-    def burn_line(self, line_buffer: list[float], between_lines: bool = False):
+        """
+        self.buffer = np.vstack([self.buffer, np.zeros(DOTS_PER_LINE)])
+
+    def burn_line(self,
+                  line_buffer: NDArray[float64],
+                  between_lines: bool = False) -> None:
         """
         Burn a line into the paper.
 
-        This is an additive process. i.e. If the line has already been burned, the new
-        line is added to the existing one. It does not replace it.
+        This is an additive process. i.e. If the line has already been burned,
+        the new line is added to the existing one. It does not replace it.
 
-        It takes 4 motor steps to move from one line to the next. The printer actually moves 2
-        steps at once, therefore if the thermal head is between 2 lines, both will be burned.
+        It takes 4 motor steps to move from one line to the next. The printer
+        actually moves 2 steps at once, therefore if the thermal head is
+        between 2 lines, both will be burned.
+
         """
-        if not between_lines:
-            self.buffer[-2] = list(map(add, self.buffer[-2], line_buffer))
+        if between_lines:
+            self.buffer[-2, :] += line_buffer
+            self.buffer[-1, :] += line_buffer
         else:
-            self.buffer[-2] = list(map(add, self.buffer[-2], line_buffer))
-            self.buffer[-1] = list(map(add, self.buffer[-1], line_buffer))
+            self.buffer[-2, :] += line_buffer
 
     def as_printout(self) -> Printout:
         """
         Generate a greyscale image from the buffer.
 
-        The method of calculating the darkness of a burned dot needs improvement.
-        """
-        data = [max(0, 255 - int(math.ceil(pixel * 25000.0)))
-                for row in self.buffer for pixel in row]
+        TODO Burn time to pixel darkness could use some work.
 
-        img = numpy.asarray(data, numpy.uint8)
-        img = numpy.reshape(img, (-1, DOTS_PER_LINE))
+        """
+        self.buffer = np.ceil((self.buffer * 2500000.0))
+
+        # Map each element between 255 and 0 where:
+        #   - 255 is white.
+        #   - 0 is black.
+        img: NDArray[uint8] = np.maximum(255 - self.buffer, 0).astype(uint8)
 
         border = int(DOTS_PER_LINE * 0.10)
         img = cv2.copyMakeBorder(
